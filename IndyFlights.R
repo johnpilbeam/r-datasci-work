e1eb398ed36e934543adf345eb7536482f000abb
johnpilbeam <jpilbeam@gmail.com>
2019-03-01 16:09:48 +0000

    Updated following completion of Week 1

--- IndyFlights.R
+++ IndyFlights.R
@@ -1,3 +1,5 @@
+# JP - 2019-03-01
+# Import CSV (Mac file path)
 myDF <- read.csv("~/Documents/r-datasci/2008.csv")
 
 head(myDF$Origin)
@@ -9,3 +11,35 @@ tail(myDF$Dest)
 head(myDF$Origin == "IND")
 
 sum(myDF$Origin == "IND")
+sum(myDF$Dest == "IND")
+
+sum(myDF$Origin == "JFK")
+sum(myDF$Dest == "JFK")
+
+sum(myDF$Origin == "ORD")
+sum(myDF$Dest == "ORD")
+
+sum((myDF$Origin == "IND") & (myDF$Dest == "ORD"))
+
+MyIndyOrigins <- subset(myDF, myDF$Origin == "IND")
+MyIndyDests <- subset(myDF, myDF$Dest == "IND")
+head(MyIndyOrigins)
+head(MyIndyDests)
+
+table(MyIndyOrigins$Month)
+plot(table(MyIndyOrigins$Month))
+plot(table(MyIndyDests$Month))
+
+sum(MyIndyOrigins$DepTime < 600, na.rm = TRUE) 
+sum(MyIndyOrigins$DepTime < 1200, na.rm = TRUE) 
+sum(MyIndyOrigins$DepTime < 1800, na.rm = TRUE) 
+sum(MyIndyOrigins$DepTime < 2400, na.rm = TRUE) 
+
+sum(is.na(MyIndyOrigins$DepTime)) 
+
+# Quiz #5
+sum((myDF$Dest == "LAX") & (myDF$Year == 2008))
+sum((myDF$Dest == "LAX") & (myDF$Origin == "ATL") & (myDF$Year == 2008))
+myATL2LAX <- subset(myDF, (myDF$Origin == "ATL") & (myDF$Dest == "LAX"))
+sum(myATL2LAX$Year == 2008)
+sum(myATL2LAX$DepTime < 1200, na.rm = TRUE) 
